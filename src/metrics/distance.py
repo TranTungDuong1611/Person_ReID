e8f789b5fb3d5fe112e825fbc13a94e947ded201
tungduong123q@gmail.com <tungduong123q@gmail.com>
2025-03-05 23:21:25 +0700

    finish distances

--- src/metrics/distance.py
+++ src/metrics/distance.py
@@ -1,8 +1,9 @@
 import numpy as np
 import torch
 import torch.nn
-import torchvision
 import torch.nn.functional as F
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def calculate_distance(outputs, target):
     """function to calculate the L2 distance between embedded vector
@@ -16,7 +17,7 @@ def calculate_distance(outputs, target):
     return dist
 
 def calculate_cosine_similarity(vector1, vector2):
-    return F.cosine_similarity(vector1, vector2, dim=1)
+    return F.cosine_similarity(vector1, vector2, dim=0)
 
 def search_by_l2_distance(query, gallery, gallery_labels):
     """ search query in a gallery
@@ -34,12 +35,9 @@ def search_by_l2_distance(query, gallery, gallery_labels):
         l2_dist = calculate_distance(query, feature_vec)
         l2_dist_lst.append(l2_dist)
         
-    _, best_idxs = torch.sort(torch.tensor(l2_dist_lst), descending=True)
-    print(f"dist: {l2_dist_lst}")
-    print(f"best idxs: {best_idxs}")
+    _, best_idxs = torch.sort(torch.tensor(l2_dist_lst), descending=False)
     best_pids = gallery_labels[best_idxs]
     best_pids = best_pids.clone().detach()
-    print(f"best pids: {best_pids}")
     return best_pids
 
 def search_by_cosine(query, gallery, gallery_labels):
@@ -71,19 +69,21 @@ def search_on_gallery_topk(queries, query_labels, gallery, gallery_labels, metri
     if metric == 'euclidean':
         for query in queries:
             pred_pids = search_by_l2_distance(query, gallery, gallery_labels)
-            batch_pred_pids.append(pred_pids)
+            batch_pred_pids.append(pred_pids.cpu())
     elif metric == 'cosine':
         for query in queries:
             pred_pids = search_by_cosine(query, gallery, gallery_labels)
-            batch_pred_pids.append(pred_pids)
+            batch_pred_pids.append(pred_pids.cpu())
             
     topk_acc = []
     for k in topk:
         batch_pred_pids = np.array(batch_pred_pids)
-        best_preds = torch.tensor(batch_pred_pids)
+        best_preds = torch.tensor(batch_pred_pids, device=device)
         correct = (best_preds[:, :k] == query_labels[:, None]).any(axis=1).sum()
+        # print(f"top {k}: {best_preds[:, :k]}")
         
         acc = correct / batch_size
+        print(f"correct: {correct} - total: {batch_size}")
         topk_acc.append(acc)
         
     return topk_acc
@@ -107,16 +107,16 @@ def search_on_gallery_map(queries, query_labels, gallery, gallery_labels, metric
     if metric == 'euclidean':
         for query in queries:
             pred_pids = search_by_l2_distance(query, gallery, gallery_labels)
-            batch_preds.append(pred_pids)
+            batch_preds.append(pred_pids.cpu())
     elif metric == 'cosine':
         for query in queries:
             pred_pids = search_by_cosine(query, gallery, gallery_labels)
-            batch_preds.append(pred_pids)
+            batch_preds.append(pred_pids.cpu())
             
     for k in topk:
         batch_preds = np.array(batch_preds)
-        batch_preds = torch.tensor(batch_preds)
-        true_pred_queries = (batch_preds[:, :k] == query_labels[:, None])
+        best_preds = torch.tensor(batch_preds, device=device)
+        true_pred_queries = (best_preds[:, :k] == query_labels[:, None])
         
         ap_lst = []
         for query in true_pred_queries:
@@ -133,26 +133,24 @@ def search_on_gallery_map(queries, query_labels, gallery, gallery_labels, metric
             ap_lst.append(ap)
         
         map = np.sum(np.array(ap_lst)) / len(ap_lst)
-        print(f"ap_lst: {ap_lst}")
         map_acc.append(map)
         
     map_acc = torch.from_numpy(np.array(map_acc))
     return map_acc
     
-    
 
-query = torch.randint(low=1, high=5, size=(4, 4), dtype=torch.float32)
-print(f"query: {query}")
-query_labels = torch.tensor([1, 2, 2, 4])
-gallery = torch.randint(low=1, high=5, size=(6, 4), dtype=torch.float32)
-print(f"gallery: {gallery}")
-gallery_labels = torch.tensor([1, 3, 2, 4, 2, 3])
+# query = torch.randint(low=1, high=5, size=(4, 4), dtype=torch.float32)
+# print(f"query: {query}")
+# query_labels = torch.tensor([1, 2, 2, 4])
+# gallery = torch.randint(low=1, high=5, size=(6, 4), dtype=torch.float32)
+# print(f"gallery: {gallery}")
+# gallery_labels = torch.tensor([1, 3, 2, 4, 2, 3])
 
 # topk_acc = search_on_gallery_topk(query, query_labels, gallery, gallery_labels)
 # print(topk_acc)
 
-map_acc = search_on_gallery_map(query, query_labels, gallery, gallery_labels)
-print(map_acc)
+# map_acc = search_on_gallery_map(query, query_labels, gallery, gallery_labels)
+# print(map_acc)
 
 # x = torch.randint(1, 3, (1, 3))
 # y = torch.randint(1, 3, (1, 3))
